f86f4f95e3c3d49cd0e92563d581c6b575713f17
vika <37137412+hi-im-vika@users.noreply.github.com>
2026-03-11 21:37:39 -0700

    Add basic draw loop

--- main.py
+++ main.py
@@ -5,7 +5,24 @@ from blessed import Terminal
 class PSMDemoApp:
     def __init__(self):
         self.term = Terminal()
-        self.spacemouse = pyspacemouse.open()
+
+    def _draw(self):
+        x = self.term.width // 2
+        y = self.term.height // 2
+
+        state = self.spacemouse.read()
+        xyz = [state.x, state.y, state.z]
+
+        label = "pyspacemouse state"
+        with self.term.location(x - len(label) // 2, y - 1):
+            print(self.term.bold_cyan(label))
+
+        for i, axis in enumerate("XYZ"):
+            with self.term.location(x - 4, y + 1 + i):
+                print(self.term.bold_white("{}: {:.2f}".format(axis, xyz[i])))
+
+        with self.term.location(x - 9, y + 5):
+            print("Press Ctrl+C to exit")
 
     def run(self):
         print("Hello from psm-demo!")
@@ -13,8 +30,7 @@ class PSMDemoApp:
               self.term.hidden_cursor(), 
               pyspacemouse.open() as self.spacemouse):
             while True:
-                print("pyspacemouse")
-                time.sleep(1)
+                self._draw()
         self.cleanup()
 
 if __name__ == "__main__":
